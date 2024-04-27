c446518a376874bf9e743c12e7ab8a701e1d20ac
Aditya Bhagat <adityabhagat525@gmail.com>
2024-04-27 11:52:24 +0530

    first commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}, err => {
-  if (err) throw err;
-  console.log('Connected to MongoDB!')
-}
-
+mongoose.connect(
+  "mongodb+srv://adi:ola@todo.you0jbe.mongodb.net/?retryWrites=true&w=majority&appName=todo" ||
+    "mongodb://localhost/fitness-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!");
+  }
 );
 
 module.exports = mongoose.connection;
